bf8eade1949d7514fc72688bee24ee2d8bbcb9a4
Dimitri <dimitri.bret@gmail.com>
2021-02-24 11:22:37 +0100

    hee

--- exec_job.sh
+++ exec_job.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
-
-# PROJECT SETTINGS, DO NOT MODIFY
-PROJECT_ID="esmo-298221"
-ZONE="europe-west1-b"
+# The instance name to run on 
 INSTANCE="esmo-1"
 
-
-# YOUR MODEL SOURCE
-MODEL_SOURCE="/c/Projects/ESMO"
-#The python job executed
+# The ESMO folder on your pc
+MODEL_SOURCE="/mnt/c/Projects/ESMO"
+#The python job executed, located in MODEL_SOURCE directory
 JOB_NAME="runscenario_cired.py"
 #The model name
 EXPERIENCE_NAME="CIRED"
 
+chmod -R +r $MODEL_SOURCE
+# gcloud compute scp --recurse $MODEL_SOURCE $INSTANCE:~/ESMO
+gcloud compute ssh $INSTANCE --command="chmod -R 777 ~/ESMO"
 
-gcloud compute scp --recurse $MODEL_SOURCE $INSTANCE:~/cplex
 
-gcloud compute ssh $INSTANCE --command= \
-    "docker run -t -i --rm \
-    -e JOB_NAME=$JOB_NAME \
-    -e EXPERIENCE_NAME=$EXPERIENCE_NAME \
-    -v $MODEL_SOURCE:/home/cplex pyplex:1.0"
+# gcloud compute ssh $INSTANCE --command= \
+#     "docker run -t -i --rm \
+#     -e JOB_NAME=$JOB_NAME \
+#     -e EXPERIENCE_NAME=$EXPERIENCE_NAME \
+#     -v $MODEL_SOURCE:/home/ESMO pyplex:1.0"
