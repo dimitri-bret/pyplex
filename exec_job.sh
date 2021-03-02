65dfb9cd2cb62cc34085c6d90b5d3b1e01721003
Dimitri <dimitri.bret@gmail.com>
2021-03-02 21:59:02 +0100

    feat: lol

--- exec_job.sh
+++ exec_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The instance name to run on 
-INSTANCE="esmo-1"
+INSTANCE="esmo-pow-1"
 
 # The ESMO folder on your pc
 MODEL_SOURCE="/mnt/c/Projects/ESMO"
@@ -10,13 +10,23 @@ JOB_NAME="runscenario_cired.py"
 #The model name
 EXPERIENCE_NAME="CIRED"
 
+EXPERIENCE_IDENTIFIER="text_experience"
+
 chmod -R +r $MODEL_SOURCE
-# gcloud compute scp --recurse $MODEL_SOURCE $INSTANCE:~/ESMO
+gcloud compute scp --recurse $MODEL_SOURCE $INSTANCE:~/ESMO
 gcloud compute ssh $INSTANCE --command="chmod -R 777 ~/ESMO"
+gcloud compute ssh $INSTANCE --command="sudo usermod -a -G docker \${USER}"
+gcloud compute ssh $INSTANCE --command="docker-credential-gcr configure-docker"
 
 
-# gcloud compute ssh $INSTANCE --command= \
+gcloud compute ssh $INSTANCE --command= \
 #     "docker run -t -i --rm \
+#     -n MIMIE_JOLIE  
 #     -e JOB_NAME=$JOB_NAME \
 #     -e EXPERIENCE_NAME=$EXPERIENCE_NAME \
-#     -v $MODEL_SOURCE:/home/ESMO pyplex:1.0"
+#     -v $MODEL_SOURCE:/home/cplex pyplex:1.0"
+
+
+
+gcloud compute ssh $INSTANCE --command=\
+"docker cp blissful_wiles:/home/cplex ~/cplex_finished_experience_identifier"
